39d56ce810b5c8a183126f6d7d786795fc1bdecc
Atheane <eroyant@gmail.com>
2020-07-10 12:20:57 +0200

    adding a fake test in static files to test if jest is called when it should

---
+++ src/__tests__/Login.js
@@ -0,0 +1,12 @@
+// const inputData = {
+//   type: "User",
+//   email: "johndoe@email.com",
+//   password: "azerty",
+//   status: "connected"
+// }
+
+test('two plus two is four', () => {
+  expect(2 + 2).toBe(4);
+});
+
+// expect()
