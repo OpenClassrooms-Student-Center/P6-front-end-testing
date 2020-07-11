837f4994272015c54240f4ec54795afe24b5a2a7
Atheane <eroyant@gmail.com>
2020-07-12 00:52:51 +0200

    better simulate login persistance

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import ROUTES  from "./constants/routes.js"
 
+let PREVIOUS_LOCATION = ''
+
 export const handleSubmitEmployee =  e => {
   localStorage.setItem("user", JSON.stringify({
     type: "Employee",
@@ -9,6 +11,7 @@ export const handleSubmitEmployee =  e => {
   }))
   e.preventDefault()
   onNavigate('/employe/note-de-frais')
+  PREVIOUS_LOCATION='/employe/note-de-frais'
   document.body.style.backgroundColor="#fff"
 }
 
@@ -21,6 +24,7 @@ export const handleSubmitAdmin = e => {
   }))
   e.preventDefault()
   onNavigate('/admin/dashboard')
+  PREVIOUS_LOCATION='/admin/dashboard'
   document.body.style.backgroundColor="#fff"
 }
 
@@ -38,16 +42,24 @@ export default () => {
     rootDiv.innerHTML = ROUTES[pathname]
   }
   
-  window.onpopstate = () => {
-    rootDiv.innerHTML = ROUTES[window.location.pathname]
+  window.onpopstate = (e) => {
+    const user = JSON.parse(localStorage.getItem('user'))
+    console.log('PREVIOUS_LOCATION', PREVIOUS_LOCATION)
+    console.log('user', user)
+
+    if (window.location.pathname === "/" && !user) {
+      document.body.style.backgroundColor="#0E5AE5"
+      rootDiv.innerHTML = ROUTES[window.location.pathname]
+    } else if (user) {
+      onNavigate(PREVIOUS_LOCATION)
+    }
   }
-  
+
   if (window.location.pathname === "/") {
     document.body.style.backgroundColor="#0E5AE5"
   }
   
   const formEmployee = document.querySelector(`form[data-testid="form-employee"]`)
-
   formEmployee.addEventListener("submit", handleSubmitEmployee)
   
   const formAdmin = document.querySelector(`form[data-testid="form-admin"]`)
