4307c54d0e0b09f6f59ba5154dea88018acdf347
Atheane <eroyant@gmail.com>
2020-07-12 17:18:35 +0200

    billsUI

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -1,4 +1,5 @@
 import VerticalLayout from './VerticalLayout.js'
+import Actions from './Actions.js'
 
 export default () => {
   const user = localStorage.getItem("user")
@@ -12,6 +13,30 @@ export default () => {
         <div class='content-header'>
           <div class='content-title'> Mes notes de Frais </div>
           <button type="button" id='btn-new-bill' class="btn btn-primary">Nouvelle note de frais</button>
+        </div>
+        <div id='data-table'>
+        <table id="example" class="table table-striped table-bordered" style="width:100%">
+        <thead>
+            <tr>
+                <th>Type</th>
+                <th>Nom</th>
+                <th>Date</th>
+                <th>Montant</th>
+                <th>Statut</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Transport</td>
+                <td>Avion Londres</td>
+                <td>12 Janv 2020</td>
+                <td>210 €</td>
+                <td>En attente</td>
+                <td>
+                  ${Actions()}
+                </td>
+            </tr>
         </div>
       </div>
     </div>`
