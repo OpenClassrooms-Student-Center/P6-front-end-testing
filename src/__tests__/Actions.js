2dcec61dbf8ccf173411eba0986a78b7bc56e70c
Atheane <eroyant@gmail.com>
2020-07-19 17:06:29 +0200

    more explicit test for Actions, testing if billUrl is in data-bill-url custom html attribute

--- src/__tests__/Actions.js
+++ src/__tests__/Actions.js
@@ -1,18 +1,22 @@
 import { screen } from "@testing-library/dom"
 import Actions from "../views/Actions.js"
+import '@testing-library/jest-dom/extend-expect'
 
-describe('Given we are connected as an employee', () => {
-  describe('When we are on Bills page and there are bills', () => {
-    test(('Then, it should render Actions even if bill file has not url'), () => {
-      const html = Actions(null)
+
+describe('Given I am connected as en Employee', () => {
+  describe('When I am on Bills page and there are bills', () => {
+    test(('Then, it should render icon eye'), () => {
+      const html = Actions()
       document.body.innerHTML = html
       expect(screen.getByTestId('icon-eye')).toBeTruthy()
     })
-    test(('Then, it should render Actions even if bill file has url'), () => {
+  })
+  describe('When I am on Bills page and there are bills with url for file', () => {
+    test(('Then, it should save given url in data-bill-url custom attribute'), () => {
       const url = '/fake_url'
       const html = Actions(url)
       document.body.innerHTML = html
-      expect(screen.getByTestId('icon-eye')).toBeTruthy()
+      expect(screen.getByTestId('icon-eye')).toHaveAttribute('data-bill-url', url)
     })
   })
 })
--- src/__tests__/ErrorPage.js
+++ src/__tests__/ErrorPage.js
@@ -1,13 +1,20 @@
 import { screen } from "@testing-library/dom"
 import ErrorPage from "../views/ErrorPage.js"
 
-describe('Given we are connected as an employee and that we already have sent at least one bill', () => {
-  describe('When we are on Bills page', () => {
-    test(('Then, it should render Actions'), () => {
-      // const fakeUrl = '/fake_url'
-      const html = Actions()
+describe('Given I am connected on app (as an Employee or an HR admin)', () => {
+  describe('When ErrorPage is called without and error in its signature', () => {
+    test(('Then, it should render ErrorPage anyway'), () => {
+      const html = ErrorPage()
       document.body.innerHTML = html
-      expect(screen.getByTestId('icon-eye')).toBeTruthy()
+      expect(screen.getAllByText('Erreur')).toBeTruthy()
+    })
+  })
+  describe('When ErrorPage is called with error message in its signature', () => {
+    test(('Then, it should render ErrorPage with its error message'), () => {
+      const error = 'Erreur de connexion internet'
+      const html = ErrorPage(error)
+      document.body.innerHTML = html
+      expect(screen.getAllByText(error)).toBeTruthy()
     })
   })
 })
