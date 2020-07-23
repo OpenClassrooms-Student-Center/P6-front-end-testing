3ae1d2431b75812f249fa947935219b817cadc53
Atheane <eroyant@gmail.com>
2020-07-24 01:40:53 +0200

    login breaking, to be debugged by student

--- src/app/format.js
+++ src/app/format.js
@@ -4,7 +4,7 @@ export const formatDate = (dateStr) => {
   const mo = new Intl.DateTimeFormat('fr', { month: 'short' }).format(date)
   const da = new Intl.DateTimeFormat('fr', { day: '2-digit' }).format(date)
   const month = mo.charAt(0).toUpperCase() + mo.slice(1)
-  return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(0,2)}`
+  return `${parseInt(da)} ${month.substr(0,3)}. ${ye.toString().substr(2,4)}`
 }
  
 export const formatStatus = (status) => {
