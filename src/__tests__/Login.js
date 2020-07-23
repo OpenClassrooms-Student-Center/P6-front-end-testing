c5beb21b7323f9596e1125a372eaa4f62509caab
Atheane <eroyant@gmail.com>
2020-07-24 01:02:16 +0200

    code is ok, tests included

--- src/__tests__/Login.js
+++ src/__tests__/Login.js
@@ -228,7 +228,7 @@ describe("Given that I am a user on login page", () => {
       })  
 
     test("It should renders HR dashboard page", () => {
-      expect(screen.getAllByText('Validations')).toBeTruthy()
+      expect(screen.queryByText('Validations')).toBeTruthy()
     })
   
   })
